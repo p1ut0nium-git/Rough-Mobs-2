bff031b314901a0f60123d711735739f0654f782
p1ut0nium-git <60378380+p1ut0nium-git@users.noreply.github.com>
2020-02-04 16:08:01 -0600

    Update to 1.14.4

---
+++ src/main/java/com/p1ut0nium/roughmobsrevamped/misc/Constants.java
@@ -0,0 +1,8 @@
+package com.p1ut0nium.roughmobsrevamped.misc;
+
+public class Constants {
+	public static final String MODID = "roughmobsrevamped";
+	public static final String MODNAME = "Rough Mobs Revamped";
+	public static final String MODVERSION = "3.0.0";
+			
+}
