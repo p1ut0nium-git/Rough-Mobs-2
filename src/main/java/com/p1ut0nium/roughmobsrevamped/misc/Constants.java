80b70903a0f2dc1c75047a0057fbfde2edc7d8af
p1ut0nium-git <60378380+p1ut0nium-git@users.noreply.github.com>
2020-02-04 16:17:05 -0600

    Delete Constants.java

--- src/main/java/com/p1ut0nium/roughmobsrevamped/misc/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.p1ut0nium.roughmobsrevamped.misc;
-
-public class Constants {
-	public static final String MODID = "roughmobsrevamped";
-	public static final String MODNAME = "Rough Mobs Revamped";
-	public static final String MODVERSION = "3.0.0";
-			
-}
