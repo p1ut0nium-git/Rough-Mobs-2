51ad044d7a3d3ff2c06300780008fba4659cec93
p1ut0nium-git <60378380+p1ut0nium-git@users.noreply.github.com>
2020-03-01 20:42:07 -0600

    Fix for #2 - Weapon switch stall
    This fix is a copy/paste from @Dynamic-Stealth by @Laike-Endaril

--- src/main/java/com/p1ut0nium/roughmobsrevamped/ai/combat/RoughAIWeaponSwitch.java
+++ src/main/java/com/p1ut0nium/roughmobsrevamped/ai/combat/RoughAIWeaponSwitch.java
@@ -3,49 +3,65 @@ package com.p1ut0nium.roughmobsrevamped.ai.combat;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 
 public class RoughAIWeaponSwitch extends EntityAIBase{
 
 	protected EntityLiving entity;
-	protected double range;
-	protected ItemStack defaultMainhand;
-	protected ItemStack defaultOffhand;
-	protected ItemStack currentMainhand;
-	protected ItemStack currentOffhand;
+	protected double rangeNear, rangeFar;
+	protected ItemStack melee, ranged;
 	
 	public RoughAIWeaponSwitch(EntityLiving entity, double range) {
-		this.entity = entity;
-		this.range = range;
-		this.defaultMainhand = entity.getHeldItemMainhand();
-		this.defaultOffhand = entity.getHeldItemOffhand();
-		setMutexBits(4);
+        this.entity = entity;
+
+        rangeNear = range * 0.9;
+        rangeFar = range * 1.1;
+
+        ranged = entity.getHeldItemMainhand();
+        melee = entity.getHeldItemOffhand();
+
+        if (melee.getItem() == Items.BOW) {
+            ItemStack swap = melee;
+            melee = ranged;
+            ranged = swap;
+        }
 	}
 	
 	@Override
 	public boolean shouldExecute() {
 		
-		if (defaultMainhand == ItemStack.EMPTY || defaultMainhand == null || defaultOffhand == ItemStack.EMPTY || defaultOffhand == null)
-			return false;
-		
-		EntityLivingBase target = entity.getAttackTarget();
-		return target != null && target.getDistanceSq(entity) < range;
+		return melee != ItemStack.EMPTY && ranged != ItemStack.EMPTY;
 	}
 	
 	@Override
 	public void startExecuting() {
 		
-		this.currentMainhand = entity.getHeldItemMainhand().copy();
-		this.currentOffhand = entity.getHeldItemOffhand().copy();
-		
-		entity.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, this.currentOffhand);
-		entity.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, this.currentMainhand);
+        EntityLivingBase target = entity.getAttackTarget();
+        if (target == null || !entity.getEntitySenses().canSee(target)) return;
+
+        double distSq = entity.getDistanceSq(target);
+        if (distSq < rangeNear)
+        {
+            if (entity.getHeldItemMainhand() == ranged)
+            {
+                entity.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, melee);
+                entity.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, ranged);
+            }
+        }
+        else if (distSq > rangeFar)
+        {
+            if (entity.getHeldItemMainhand() == melee)
+            {
+                entity.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, ranged);
+                entity.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, melee);
+            }
+        }
 	}
 	
-	@Override
-	public void resetTask() {
-		entity.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, this.currentMainhand);
-		entity.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, this.currentOffhand);
-	}
+    @Override
+    public boolean shouldContinueExecuting() {
+        return false;
+    }
 }
